91f776290ae12761e8b4e0def2c3b6f129bbb1a1
egor <scudf24@gmail.com>
2025-07-19 11:43:48 +0300

    created recipient, message, mailing, attempt, user models, added urls, created createadmin command

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
+
 # Create your models here.
+class Recipient(models.Model):
+    email = models.EmailField(unique=True, verbose_name="Email")
+    full_name = models.CharField(max_length=100, null=True, blank=True, verbose_name="ФИО")
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Получатель рассылки"
+        verbose_name_plural = "Получатели рассылки"
+        ordering = ["email"]
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=100, null=True, blank=True, verbose_name="Тема письма")
+    body = models.TextField(null=True, blank=True, verbose_name="Тело письма")
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
+        ordering = ["subject"]
+
+
+class Mailing(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    finished_at = models.DateTimeField(auto_now=True)
+    STATUS_CHOICES = [
+        ("created", "Created"),
+        ("published", "Published"),
+        ("completed", "Completed"),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="created", verbose_name="Статус рассылки")
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="messages", verbose_name="Сообщение")
+    recipient = models.ManyToManyField(Recipient, related_name="recipients", verbose_name="Получатели")
+
+    def __str__(self):
+        return f"{self.message}, {self.created_at}, {self.finished_at}"
+
+    class Meta:
+        verbose_name = "Рассылка"
+        verbose_name_plural = "Рассылки"
+        ordering = ["finished_at"]
+
+
+class Attempt(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    STATUS_CHOICES = [
+        ("successfully", "Successfully"),
+        ("unsuccessfully", "Unsuccessfully"),
+    ]
+    status = models.CharField(max_length=14, choices=STATUS_CHOICES, default="unsuccessfully")
+    server_response = models.TextField()
+    recipient = models.ForeignKey(Mailing, on_delete=models.CASCADE, related_name="mailings", verbose_name="Рассылка")
+
+    def __str__(self):
+        return f"{self.recipient}, {self.created_at}, {self.status}"
+
+    class Meta:
+        verbose_name = "Попытка рассылки"
+        verbose_name_plural = "Попытки рассылки"
+        ordering = ["created_at"]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+
+    avatar = models.ImageField(
+        upload_to="avatars/", verbose_name="Аватар", blank=True, null=True, help_text="Загрузите свой аватар"
+    )
+    phone = models.CharField(
+        max_length=35, verbose_name="Телефон", blank=True, null=True, help_text="Введите свой номер телефон"
+    )
+    country = models.CharField(
+        max_length=50, verbose_name="Страна", blank=True, null=True, help_text="Введите свою страну"
+    )
+    token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
