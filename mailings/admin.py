da940b25e28c0841c5bd1d78d667ffe9fdde4908
egor <scudf24@gmail.com>
2025-07-24 19:22:26 +0300

    added classes for admin panel

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Attempt, Mailing, Message, Recipient
+
+
+@admin.register(Recipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "full_name")
+    list_filter = ("full_name",)
+    search_fields = (
+        "email",
+        "full_name",
+    )
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "subject", "body")
+    list_filter = ("subject",)
+    search_fields = ("subject",)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "status", "message")
+    list_filter = ("created_at", "status")
+    search_fields = ("message",)
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at", "status", "server_response")
+    list_filter = ("created_at", "status")
+    search_fields = ("server_response",)
